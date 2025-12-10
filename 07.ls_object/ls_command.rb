026b998214d802818ddb4085b8ba98becdf1818d
horishota6566 <shou6130@gmail.com>
2025-12-10 11:19:36 +0900

    refactor(ls_command): fetch_file_namesで@paramsを参照するように変更

--- 07.ls_object/ls_command.rb
+++ 07.ls_object/ls_command.rb
@@ -11,7 +11,7 @@ require_relative 'short_list'
 class LsCommand
   def initialize
     @params = parse_params
-    @file_names = fetch_file_names(@params)
+    @file_names = fetch_file_names
     @file_infos = @params[:l] ? @file_names.map { |name| FileInfo.new(name) } : nil
   end
 
@@ -36,13 +36,13 @@ class LsCommand
     params
   end
 
-  def fetch_file_names(params)
+  def fetch_file_names
     pattern = '*'
-    flags = params[:a] ? File::FNM_DOTMATCH : 0
+    flags = @params[:a] ? File::FNM_DOTMATCH : 0
 
     file_names = Dir.glob(pattern, flags).sort
 
-    params[:r] ? file_names.reverse : file_names
+    @params[:r] ? file_names.reverse : file_names
   end
 end
 
